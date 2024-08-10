6b85daf5f16cf97023a5e01a68e559d7682d5a94
kimys <ys2life1@naver.com>
2024-08-10 14:45:58 +0900

    꽁머니페이지 멘트 수정

--- app/guide/ggong/page.tsx
+++ app/guide/ggong/page.tsx
@@ -20,21 +20,21 @@ function page() {
             <span className="text-blue">꽁머니 </span>바로알기
           </div>
           <p className="w-full max-w-[900px] px-5 pt-5 text-description leading-6 flex flex-col justify-center items-center text-sm">
-            온라인 토토사이트를 이용하시는 분들은 꽁머니에 대해서 잘 알고 계실
-            것 입니다. 꽁머니는 온라인 토토사이트에서 플레이어들을 끌어내기
-            위하여 주로 사용하는 마케팅 전략이자 이벤트인데요. 꽁머니를 준다고
-            하여 모든 사이트를 이용해서는 안 된다는 점 기억하셔야 합니다. 그
-            이유는 꽁머니 이벤트로 고객을 유치한 후에 먹튀를 하는 사이트들이
-            늘고 있기 때문인데요. 꽁머니를 받았다고 해도 먹튀 피해를 입게 된다면
-            그 피해가 고스란히 플레이어 책임이 되기 때문에 신중해야 합니다.
-            그렇다면 꽁머니를 지급하는 온라인 토토사이트 중에 어떤 사이트를
-            이용해야 안심하고 이용이 가능할까요? 함께 알아보겠습니다.
+            꽁머니는 토토사이트나 스포츠토토, 카지노에서 신규 가입자나 기존
+            사용자에게 제공하는 무료 돈입니다. 이를 통해 사용자는 실제 돈을
+            사용하기 전에 게임을 시험해볼 수 있습니다. 꽁머니는 주로
+            메이저사이트와 안전한놀이터에서 제공되며, 먹튀검증이 완료된 신뢰할
+            수 있는 사이트에서 주어지는 것이 일반적입니다. 하지만 꽁머니를 통해
+            안전하게 이용을 마쳤더라도 안전한 업체라고 안심할 수는 없습니다.
+            마케팅 목적으로 이용자를 끌어들인 후 먹튀를 하는 업체도 심심치 않게
+            존재하기 때문입니다. 꽁머니팡에서 활발한 소통을 통하여 피해 사례를
+            공유하고 안전한 업체를 찾아보세요.
           </p>
           <div className="w-full max-w-[900px] pt-20 px-5 flex items-center justify-center">
             <section className="hidden lg:flex w-1/2 h-full  flex-col justify-center items-center">
               <div className="font-bold text-center">
-                <h1 className="text-2xl ">안전한 꽁머니</h1>
-                <h1 className="text-2xl ">사이트 찾는 방법</h1>
+                <h1 className="text-2xl ">꽁머니</h1>
+                <h1 className="text-2xl ">이용 주의사항</h1>
               </div>
               <Image
                 alt="꽁머니팡소개"
@@ -45,58 +45,63 @@ function page() {
             </section>
             <section className="w-full flex flex-col gap-5 ">
               <div className="flex flex-col md:flex-row gap-2 w-full justify-center items-center">
-                <article className="w-full max-w-80 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
+                <article className="w-full h-36 max-w-80  relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
                   <div>
                     <p className="">
-                      안전한 꽁머니를 찾고 싶다면 먹튀검증 업체에 등록된
-                      업체인지를 확인하세요.
+                      꽁머니는 게임을 시험해볼 수 있는 기회를 제공합니다.
+                      이용자들은 프로토나 파워볼을 무료로 시도해볼 수 있으며,
+                      이를 통해 안전놀이터 인지 판단할 수 있습니다.
                     </p>
                   </div>
                 </article>
-                <article className="w-full max-w-80 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
+                <article className="w-full h-36 max-w-80  relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
                   <div>
                     <p className="">
-                      꽁머니팡은 꽁머니를 지급하는 사이트 중에서도 검증된
-                      사이트만을 추천해 드립니다.
-                    </p>
-                  </div>
-                </article>
-              </div>
-              <div className="flex flex-col md:flex-row gap-2 w-full justify-center items-center">
-                <article className="w-full max-w-80 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
-                  <div>
-                    <p className="">
-                      꽁머니는 신규가입머니 지급을 의미하는 것으로 플레이어를
-                      유인하기 위한 방법입 니다. 그렇기 때문에 꽁머니를 지나치게
-                      많이 주는 사이트는 경계해야 합니다. 일반적으로 가입머니는
-                      최대 3만원 정도 까지이며 더 많은 꽁머니를 주는 사이트가
-                      있다면이용에 주의해 주세요
-                    </p>
-                  </div>
-                </article>
-                <article className="w-full md:h-[182px] max-w-80 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
-                  <div>
-                    <p className="">
-                      안전한 꽁머니를 찾고 싶다면 먹튀검증 업체에 등록된
-                      업체인지를 확인하세요.
+                      신규 토토사이트나 스포츠토토를 이용할 때, 꽁머니를 통해
+                      사이트의 인터페이스와 게임 종류를 체험할 수 있습니다. 이를
+                      통해 해당 사이트가 메이저놀이터인지 확인할 수 있습니다.
                     </p>
                   </div>
                 </article>
               </div>
               <div className="flex flex-col md:flex-row gap-2 w-full justify-center items-center">
-                <article className="w-full max-w-80 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
+                <article className="w-full max-w-80 h-36 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
                   <div>
                     <p className="">
-                      안전한 꽁머니를 찾고 싶다면 먹튀검증 업체에 등록된
-                      업체인지를 확인하세요.
+                      꽁머니로 얻은 승리는 종종 실제 수익화가 가능할 수
+                      있습니다. 다만, 그렇다고 안전한 업체로 확실히 믿을 수는
+                      없습니다. 오랜기간 소액으로 이용하면서 안전놀이터인지
+                      검증을 거쳐야 합니다.
                     </p>
                   </div>
                 </article>
-                <article className="w-full max-w-80 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
+                <article className="w-full max-w-80 h-36 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
                   <div>
                     <p className="">
-                      안전한 꽁머니를 찾고 싶다면 먹튀검증 업체에 등록된
-                      업체인지를 확인하세요.
+                      모든 사이트가 신뢰할 수 있는 것은 아니며, 먹튀사이트가
+                      있을 수 있습니다. 따라서, 꽁머니를 제공하는 사이트가
+                      먹튀검증을 통과했는지 확인하는 것이 중요합니다.
+                    </p>
+                  </div>
+                </article>
+              </div>
+              <div className="flex flex-col md:flex-row gap-2 w-full justify-center items-center">
+                <article className="w-full max-w-80 h-36 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
+                  <div>
+                    <p className="">
+                      꽁머니는 특정 게임이나 안전한놀이터에서만 사용이 가능하며,
+                      사용 조건이 제한적일 수 있습니다. 또한, 사용한 금액이나
+                      배당에 따른 제한이 있을 수 있습니다.
+                    </p>
+                  </div>
+                </article>
+                <article className="w-full max-w-80 h-36 relative px-5 py-5 text-sm rounded-lg  border border-solid border-[#EEEEEE] flex items-center justify-center">
+                  <div>
+                    <p className="">
+                      꽁머니를 사용하려면 업체마다 복잡한 베팅 요구 사항과
+                      규정이 있습니다. 이러한 요구 사항을 충족하지 않으면 실제
+                      수익화가 어려울 수 있습니다. 예를 들어, 스포츠토토의 경우
+                      특정 베팅 규칙을 따라야 할 수 있습니다.
                     </p>
                   </div>
                 </article>
@@ -112,12 +117,12 @@ function page() {
             ></Image>
             <div className="w-full max-w-[900px] text-description leading-6">
               <p>
-                꽁머니팡은 꽁머니를 지급하는 사이트에 대해서 더 신중하고
-                정확하게 검증하기 위해 노력하고 있습니다.그렇기에 꽁머니
-                사이트라고 해서 먹튀 우려가 있는 사이트를 추천해 드리지 않는다고
-                확실히 말씀 드릴 수 있습니다.따라서 꽁머니 사이트를 이용하기
-                이전에 토토핫에 추천되어 있는 사이트인지를 확인한 후에
-                이용하시는 것이 좋겠습니다.
+                꽁머니를 안전히 사용하기 위해서는 이러한 여러가지 주의 사항이
+                필요합니다. 꽁머니팡은 꽁머니를 지급하는 사이트를 여러 유저들의
+                사용경험, 피해사례를 통하여 검증합니다. 또 한번 안전한 업체라고
+                판단이 되어도 언제나 안전한 업체일수는 없으므로 일정 기간마다
+                검증을 반복합니다. 그렇기에 꽁머니팡에서 추천드리는 업체는
+                안전한 업체임을 신뢰할 수 있습니다.
               </p>
             </div>
           </article>
